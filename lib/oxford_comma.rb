e374364014b03e9e817ef45dc628e7690de9cf87
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-27 18:34:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
 if array.length ==1 
-  return array 
+  puts array 
 elsif array.length ==2 
-return array.join("and")
+puts array.join("and")
 elsif array.length>=3
-array.insert(-2,"and")
-array.join(",")
-return 
+array.insert(-2,"and")!
+puts array.join(",")
 end
 end
